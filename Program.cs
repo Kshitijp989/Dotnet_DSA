438f5df591242814ee5a5c9d54d97004f6ca4425
ShetejPatil989 <shetej.patil@neosoftmail.com>
2024-06-07 11:16:11 +0530

    Added stringmanupilaion

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotNet_DSA;
+using DotNet_DSA.StringManupilation;
 
 Console.WriteLine("Hello, World!");
 int[] arr =  { 3, 2, 4 };
@@ -13,6 +14,13 @@ int[] miss = { 1, 2, 3, 5 };
 MissingNumber missing = new MissingNumber();
 int ans= missing.MissingNumberFromArray(miss, 5);
 Console.WriteLine(ans);
+
+
+//---------------------------------------------------------------------------------------------------------------
+RemoveDuplicateCharacterFrom_String removeDuplicateCharacterFrom_String = new RemoveDuplicateCharacterFrom_String();
+
+Console.WriteLine(removeDuplicateCharacterFrom_String.RemoveDuplicateChar("hello"));
+Console.WriteLine(removeDuplicateCharacterFrom_String.RemoveDuplicateChar1("hello1"));
 
 
 
---
+++ StringManupilation/RemoveDuplicateCharacterFrom String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_DSA.StringManupilation
+{
+    public  class RemoveDuplicateCharacterFrom_String
+    {
+
+        public string RemoveDuplicateChar(string str)
+        {
+            return new string(str.ToCharArray().Distinct().ToArray());
+        }
+        public string RemoveDuplicateChar1(string str)
+        { string newString = "";
+           List <char> list = new List <char>();
+            foreach (char c in str) {
+                if (list.Contains(c))
+                {
+                    continue;
+                }
+                list.Add(c);
+                newString += c;
+            }
+            return newString;
+
+        }
+
+    }
+}
